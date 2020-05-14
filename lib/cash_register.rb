a08548822f1a3e849c8291fcf53824f098de3192
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-14 23:17:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -38,10 +38,7 @@ class CashRegister
   end
 
   def void_last_transaction
-    if @cart.empty?
-      @total = 0
-    else
-      @total - @last_transaction
+      @total -= @last_transaction
     end
   end
 end
